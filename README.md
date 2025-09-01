29f74720b6f5f81e37b2d54efdfc170f5c8b9a2e
silverstillisntgold <nickdilla13@gmail.com>
2025-09-01 18:20:11 -0400

    misc

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# THIS IS A TEST :D
+# Testing
+
+A simple repo for experimenting with building binaries for multiple targets with different target features.
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use fastxfix::CommonStr;
+use std::iter;
 use ya_rand::encoding::{Base16, Encoder};
 use ya_rand::{Generator, SecureGenerator, SecureRng};
 
 /// HAHAHA FUNNY NUMBER HAHAHA
-const STRING_COUNT: usize = 69420;
+const STRING_COUNT: usize = 69;
 
 fn main() {
     let mut rng = SecureRng::new();
-    let mut v = vec![String::default(); STRING_COUNT];
-    v.fill_with(|| rng.text::<Base16>(Base16::MIN_LEN));
+    let v = Vec::from_iter(iter::repeat_n(
+        rng.text::<Base16>(Base16::MIN_LEN),
+        STRING_COUNT,
+    ));
     match v.common_prefix_ref() {
         Some(s) => println!("common prefix of '{}' was randomly generated", s),
         None => println!("no common prefix was randomly generated"),
